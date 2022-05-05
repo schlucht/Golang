78a0666822d11426530434cd3fcba11dbe095f45
schlucht <web@jagolo.ch>
2022-05-05 16:42:46 +0000

    Reqest and Response

--- Beginners/src/website/main.go
+++ Beginners/src/website/main.go
@@ -4,30 +4,52 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
+	"strings"
 )
+
+var funcMap = template.FuncMap{
+	"Upper":   strings.ToUpper,
+	"Multipl": Multipl,
+}
+
+var tpl *template.Template
 
 type Page struct {
 	Title string
+	P     float32
 }
 
+func Multipl(p float32) (str string) {
+	return fmt.Sprintf("%.2f", p*5)
+}
+
+var p = Page{
+	Title: "Lothar",
+	P:     23.6,
+}
+var err template.Error
+
 func main() {
-	http.HandleFunc("/hello", helloHandleFunc)
+	tpl = template.New("index.html")
+	tpl = tpl.Funcs(funcMap)
+	tpl = template.Must(tpl.ParseFiles("templates/index.html"))
+	http.HandleFunc("/", indexHandler)
+	http.HandleFunc("/login", loginHandler)
+	http.HandleFunc("/contact", contactHandler)
+
 	fmt.Println("Server läuft Port 8080")
 	http.ListenAndServe(":8080", nil)
 }
 
-func helloHandleFunc(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.templ.html")
-	if err != nil {
-		fmt.Fprint(w, err.Error(), "helloHandleFunc")
-	}
-	tmpl.Execute(w, Page{Title: "Lothar"})
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	tpl.Execute(w, p)
+}
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	tpl2 := template.Must(template.ParseFiles("templates/login.templ.html"))
+	tpl2.Execute(w, nil)
 }
 
-func aboutHandleFunc(w http.ResponseWriter, r *http.Request) {
-	tmpl, err := template.ParseFiles("templates/index.templ.html")
-	if err != nil {
-		fmt.Fprint(w, err.Error(), "helloHandleFunc")
-	}
-	tmpl.Execute(w, Page{Title: "Lothar"})
+func contactHandler(w http.ResponseWriter, r *http.Request) {
+	tpl2 := template.Must(template.ParseFiles("templates/contact.templ.html"))
+	tpl2.Execute(w, nil)
 }
